6c001b0a31f75bf4ed85f1f2dec961c1f412a5ed
KKHdu <1354333986@qq.com>
2017-07-06 14:05:19 +0800

    mark

--- src/main/java/betahouse/controller/DemoController.java
+++ src/main/java/betahouse/controller/DemoController.java
@@ -19,6 +19,7 @@ public class DemoController extends BaseController {
     }
     @RequestMapping("/hi")
     public String hi(HttpServletRequest request, HttpServletResponse response, Model model){
+
         return "index/index";
     }
 

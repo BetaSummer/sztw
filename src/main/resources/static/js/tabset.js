587a47dbe4fddf035cfc19ccf16fbf34af6edce2
toukajiang <493365903@qq.com>
2017-07-09 15:20:13 +0800

    为index页面的分级导航增添及美化了移动web界面

--- src/main/resources/static/js/tabset.js
+++ src/main/resources/static/js/tabset.js
@@ -1,16 +1,32 @@
 /**
  * Created by Administrator on 2017/4/27.
  */
-$(function () {
-   $(".sub-ft").click(function () {
-       $(this).next().toggle(500);
-   });
-});
+
+
 
 $(function () {
-   $(".sub-nav").find("li").click(function () {
-       $(".sub-nav").find("li").removeClass("on");
-       $(this).addClass("on");
-       $(".content-nav").html($(this).parent().parent().prev().html()+"/ "+$(this).html());
-   }) ;
-});
+    var width = window.screen.width;
+    if (width<768){
+        $(function () {
+            $(".sub-ft").click(function () {
+                $(".sub-ft").toggle();
+                $(this).toggle();
+                $(this).next().toggle(500);
+            });
+        });
+    }
+    else {
+        $(function () {
+            $(".sub-ft").click(function () {
+                $(this).next().toggle(500);
+            });
+        });
+        $(function () {
+            $(".sub-nav").find("li").click(function () {
+                $(".sub-nav").find("li").removeClass("on");
+                $(this).addClass("on");
+                $(".content-nav").html($(this).parent().parent().prev().html()+"/ "+$(this).html());
+            }) ;
+        });
+    }
+})

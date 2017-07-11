5319b3deedb28a4f7828c146585c6ac582f024a6
5rKB5bPlusD <x1654102203@gmail.com>
2017-07-11 14:15:10 +0800

    优化数据表结构 (#19)

---
+++ src/main/java/betahouse/mapper/PowerMapper.java
@@ -0,0 +1,18 @@
+package betahouse.mapper;
+
+import betahouse.model.Power;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public interface PowerMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Power record);
+
+    Power selectByPrimaryKey(Integer id);
+
+    List<Power> selectAll();
+
+    int updateByPrimaryKey(Power record);
+}

a86bc1caf41e5c123273830a78df05f4d636a143
slnm <493365903@qq.com>
2017-07-12 15:15:30 +0800

    页面根据权限显示 (#21)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,34 +6,13 @@
 /****************************************/
 
 $(function () {
-    $.ajax({
-        type: "POST",
-        url: "/user/getLv",
-        dataType:"json",
-        success: function(r){
-            if(r.data>1){
-                $("#form-view").after("<li class='sub-item' id='form-manage'>管理</li>");
-                $("#form-view").hide();
-                $("#form-appli").hide();
-                $("#form-manage").click(function () {
-                    $(".loading").show();
-                    $.get("/approveForm/listClubActivity",function () {
-                        $(".content-body").load("/approveForm/listClubActivity",function () {
-                            $(".loading").hide();
-                        });
-                    });
-                });
-                //re creat js(because new html code has not events)
-                $(".sub-nav").find("li").click(function () {
-                    $(".sub-nav").find("li").removeClass("on");
-                    $(this).addClass("on");
-                    $(".content-nav").html($(this).parent().parent().prev().html()+"/ "+$(this).html());
-                }) ;
-            }
-        },
-        error: function(data){
-
-        }
+    $("#form-manage").click(function () {
+        $(".loading").show();
+        $.get("/approveForm/listClubActivity",function () {
+            $(".content-body").load("/approveForm/listClubActivity",function () {
+                $(".loading").hide();
+            });
+        });
     });
 });
 /* login */

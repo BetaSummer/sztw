fe44f3b16cf2ffb793ef789931a093a5ac37484d
KKHdu <1354333986@qq.com>
2017-07-13 23:07:26 +0800

    "信息管理部分界面"

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -71,19 +71,31 @@ $(function () {
 $(function () {
     $("#f-list").click(function () {
         // $(".content-body").html("");
-        $("#f-flow").show();$("#manage-a-show").hide();$("#public-news-").hide();
+        $("#f-flow").show();$("#manage-a-show").hide();$("#public-news-").hide();$("#president-management").hide();$("#club-finance").hide()
     });
 });
 $(function () {
     $("#manage-a").click(function () {
         // $(".content-body").html("");
-        $("#manage-a-show").show();$("#f-flow").hide();$("#public-news-").hide();
+        $("#manage-a-show").show();$("#f-flow").hide();$("#public-news-").hide();$("#president-management").hide();$("#club-finance").hide()
+    });
+});
+$(function () {
+    $("#-president-management").click(function () {
+        // $(".content-body").html("");
+        $("#president-management").show();$("#f-flow").hide();$("#public-news-").hide();$("#manage-a-show").hide();$("#club-finance").hide()
     });
 });
 $(function () {
     $("#public-news").click(function () {
         // $(".content-body").html("");
-        $("#public-news-").fadeIn();$("#f-flow").hide();$("#manage-a-show").hide();
+        $("#public-news-").fadeIn();$("#f-flow").hide();$("#manage-a-show").hide();$("#president-management").hide();$("#club-finance").hide()
+    });
+});
+$(function () {
+    $("#-club-finance").click(function () {
+        // $(".content-body").html("");
+        $("#club-finance").fadeIn();$("#public-news-").hide();$("#f-flow").hide();$("#manage-a-show").hide();$("#president-management").hide();
     });
 });
 

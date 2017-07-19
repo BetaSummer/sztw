8d87ad5c1b73f513aa7b2fca8db99561922154f7
toukajiang <493365903@qq.com>
2017-07-19 17:53:42 +0800

    用户管理 弹窗 完成

--- src/main/resources/static/js/manage.js
+++ src/main/resources/static/js/manage.js
@@ -2,13 +2,12 @@
  * Created by Administrator on 2017/7/19.
  */
 
-//manage-btn
+//financeB
 
 function writeInformation(index) {
-    if(index!=0){
+    if(index){
         var trTemp = $('#fin-table tr:eq(' + index + ')');
-        $(".manage-alert-title").after("<tr class='manage-alert-in delete-tr'><td>"+trTemp.children('td').eq(1).html()+"</td><td>"+trTemp.children('td').eq(2).html()+"</td>><td>"+trTemp.children('td').eq(3).html()+"</td><td> <select> <option value='1'>增加</option> <option value='-1'>减少</option> </select> </td> <td> <select> <option value='1'>预留</option> <option value='2'>自留</option> </select> </td> <td><input type='text' name='moneychange' placeholder='人民币（元）'/></td></tr>")
-
+        $(".manage-alert-title").after("<tr class='manage-alert-in delete-tr'><td>"+trTemp.children('td').eq(1).html()+"</td><td>"+trTemp.children('td').eq(2).html()+"</td><td>"+trTemp.children('td').eq(3).html()+"</td><td> <select> <option value='1'>增加</option> <option value='-1'>减少</option> </select> </td> <td> <select> <option value='1'>预留</option> <option value='2'>自留</option> </select> </td> <td><input type='text' name='moneychange' placeholder='人民币（元）'/></td></tr>")
     }
 }
 $(function () {
@@ -24,6 +23,33 @@ $(function () {
             if($(this).is(':checked')){
                 var index = $(this).parent().parent().index();
                 writeInformation(index);
+            }
+        });
+        $(".manage-alert").show(200);
+    });
+});
+
+//userManage
+function writeInformation2(index) {
+    if(index){
+        var trTemp = $('#fin-table tr:eq(' + index + ')');
+        $(".manage-alert-title").after("<tr class='manage-alert-in delete-tr'><td style='display: none;'>"+trTemp.children('td').eq(2).html()+"</td><td>"+trTemp.children('td').eq(5).html()+"</td><td>"+trTemp.children('td').eq(4).html()+"</td><td><input type='text' name='moneychange' placeholder='修改密码'/></td><td><input type='text' name='moneychange' placeholder='联系方式' value='"+trTemp.children('td').eq(3).html()+"'/></td> <td><input type='text' name='moneychange' placeholder='邮箱' value='"+trTemp.children('td').eq(1).html()+"'/></td></tr>")
+
+    }
+}
+$(function () {
+    $(".manage-btn2").click(function () {
+        $(".delete-tr").remove();
+        var index = $(this).parent().parent().index();
+        writeInformation2(index);
+        $(".manage-alert").show(200);
+    });
+    $("#manageAll2").click(function(){
+        $(".delete-tr").remove();
+        $("input[type='checkbox']").each(function(){
+            if($(this).is(':checked')){
+                var index = $(this).parent().parent().index();
+                writeInformation2(index);
             }
         });
         $(".manage-alert").show(200);

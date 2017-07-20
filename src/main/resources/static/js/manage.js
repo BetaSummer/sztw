8b1368f102dca4f822152fe3b53d2801276ab38e
toukajiang <493365903@qq.com>
2017-07-20 17:28:44 +0800

    完成除通知/公告的适配

--- src/main/resources/static/js/manage.js
+++ src/main/resources/static/js/manage.js
@@ -7,7 +7,7 @@
 function writeInformation(index) {
     if(index){
         var trTemp = $('#fin-table tr:eq(' + index + ')');
-        $(".manage-alert-title").after("<tr class='manage-alert-in delete-tr'><td>"+trTemp.children('td').eq(1).html()+"</td><td>"+trTemp.children('td').eq(2).html()+"</td><td>"+trTemp.children('td').eq(3).html()+"</td><td> <select> <option value='1'>增加</option> <option value='-1'>减少</option> </select> </td> <td> <select> <option value='1'>预留</option> <option value='2'>自留</option> </select> </td> <td><input type='text' name='moneychange' placeholder='人民币（元）'/></td><td style='display: none;'>"+trTemp.children('td').eq(4).html()+"</td></tr>");
+        $(".manage-alert-title").after("<tr class='manage-alert-in delete-tr'><td>"+trTemp.children('td').eq(1).html()+"</td><td>"+trTemp.children('td').eq(2).html()+"</td><td>"+trTemp.children('td').eq(3).html()+"</td><td> <select> <option value='1'>增加</option> <option value='-1'>减少</option> </select> </td> <td> <select> <option value='1'>预留</option> <option value='2'>自留</option> </select> </td> <td><input type='text' name='moneychange' placeholder='（元）'/></td><td style='display: none;'>"+trTemp.children('td').eq(4).html()+"</td></tr>");
     }
 }
 $(function () {
@@ -28,6 +28,53 @@ $(function () {
         $(".manage-alert").show(200);
     });
 });
+
+$(function () {
+    $("#finance-btn").click(function () {
+        var dataLength = $(".manage-alert-in").length;
+        var dataTrArr = $(".manage-alert-in");
+        var data = new Array(dataLength-1);
+        var comment = $("#comment").val();
+        for(var i = 0;i<dataLength-1;i++){
+            var id = dataTrArr[i].children[6].innerHTML;
+            var change = dataTrArr[i].children[3].firstElementChild.value;
+            var selfReserve = dataTrArr[i].children[4].firstElementChild.value;
+            var money = dataTrArr[i].children[5].firstElementChild.value;
+            data[i]={
+                "id":id,
+                "change":change,
+                "selfReserve":selfReserve,
+                "money":money
+            }
+        }
+
+        var dataAll = {
+            "data":{data:JSON.stringify(data)},
+            "comment":comment
+        }
+        $.ajax({
+            type: "GET",
+            traditional:true,
+            url: "/finance/changClubFinance",
+            data: dataAll,
+            dataType:"json",
+            success: function(r){
+                alert(r.message);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+                alert(XMLHttpRequest.status);
+                alert(XMLHttpRequest.readyState);
+                alert(textStatus);
+            }
+        });
+    });
+});
+
+
+
+
+
+
 
 //userManage
 function writeInformation2(index) {
@@ -56,6 +103,8 @@ $(function () {
     });
 });
 
+
+
 //checkAll
 $(function () {
     $("#checkall").click(function(){
@@ -75,6 +124,9 @@ $(function () {
     });
 });
 
+
+
+
 //out
 $(function () {
     $(".manage-out").click(function () {
@@ -82,43 +134,3 @@ $(function () {
     });
 });
 
-//finance
-$(function () {
-    $("#finance-btn").click(function () {
-        var dataLength = $(".manage-alert-in").length;
-        var dataTrArr = $(".manage-alert-in");
-        var data = new Array(dataLength-1);
-        var comment = $("#comment").val();
-        for(var i = 0;i<dataLength-1;i++){
-            var id = dataTrArr[i].children[6].innerHTML;
-            var change = dataTrArr[i].children[3].firstElementChild.value;
-            var selfReserve = dataTrArr[i].children[4].firstElementChild.value;
-            var money = dataTrArr[i].children[5].firstElementChild.value;
-            data[i]={
-                "id":id,
-                "change":change,
-                "selfReserve":selfReserve,
-                "money":money
-            }
-        }
-        dataAll = {
-            "data":data,
-            "comment":comment
-        }
-        console.log(data);
-        $.ajax({
-            type: "GET",
-            url: "/finance/changClubFinance",
-            data: dataAll,
-            dataType:"json",
-            success: function(r){
-                alert(r.message);
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown){
-                alert(XMLHttpRequest.status);
-                alert(XMLHttpRequest.readyState);
-                alert(textStatus);
-            }
-        });
-    });
-});

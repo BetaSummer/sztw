ad9287c2b76ef35ee9a742bf20d591e2ea0ebdbd
ljy <493365903@qq.com>
2017-10-01 06:35:13 +0800

    one problem showed in file main.js line 317( TODO ) (#70)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -193,24 +193,6 @@ $(function () {
         }) ;
     });
 
-    $(".deleteMessage").click(function () {
-        var id = $(this).attr("content");
-        $(".content-body").html("");
-        $(".loading").show();
-        $(".content-body").load("/information/deleteAnnouncementById?id="+id,function () {
-            $(".loading").hide();
-        });
-    });
-
-    $(".editMessage").click(function () {
-        var id = $(this).attr("content");
-        $(".content-body").html("");
-        $(".loading").show();
-        $(".content-body").load("/information/editAnnouncementById?id="+id,function () {
-            $(".loading").hide();
-        });
-    });
-});
 $(".deleteMessage").click(function () {
     var r = confirm("确定要删除吗？");
     if(r==true){
